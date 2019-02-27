a5a15e8f35f8fd91c714d521e8352e8550bdd47c
Acer <w.47@qq.com>
2019-02-27 22:44:34 +0800

    fix 20190227

--- src/main/java/com/bo/springmvc/controller/HelloWorld.java
+++ src/main/java/com/bo/springmvc/controller/HelloWorld.java
@@ -1,6 +1,7 @@
 package com.bo.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -26,4 +27,33 @@ public class HelloWorld {
 		mv.addObject("message", "欢迎回来");
 		return mv;
 	}
+	
+	// SpringMVC允许直接返回一个String数据，作为视图名称。不需要数据模型。
+	@RequestMapping(value = "test3")
+	public String test01() {
+		// 如果只是访问页面，就不需要ModelAndView对象，直接返回一个字符串即可
+		return "index"; // 访问的是index.jsp页面
+	}
+
+	// 也可以把视图和模型分离，只需要Model中的数据
+	@RequestMapping(value = "test5")
+	public String test02(Model model) {
+		model.addAttribute("msg", "我试试Model好不好用");
+		return "index"; // 访问的是index.jsp页面
+	}
+	
+	// 演示：重定向
+	@RequestMapping("/showRedirect")
+	public String showRedirect(){
+		// 返回值以redirect:开头就是重定向，但是后面必须跟上URI路径而不是视图名
+		return "redirect:/test/showRedirect.jsp";
+	}
+
+	// 演示：请求转发
+	@RequestMapping("/showForward")
+	public String showForward(){
+		// 返回值以forward:开头就是请求转发，但是后面必须跟上URI路径而不是视图名
+		return "forward:/test/showForward.jsp";
+	}
+	
 }
